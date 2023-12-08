9b5687cb94f2e3faba0752455316d84181e35193
Darius Calugar <darius.calugar@rebeldot.com>
2023-12-08 21:31:52 +0200

    Separated example 3

--- take_off_with_flutter/lib/screens/showcase_screen.dart
+++ take_off_with_flutter/lib/screens/showcase_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:take_off_with_flutter/flutter_examples/example_4/widgets/navigation_drawer.dart';
 
 class ShowcaseScreen extends StatelessWidget {
   const ShowcaseScreen({super.key});
@@ -7,7 +6,6 @@ class ShowcaseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const AppDrawer(),
       appBar: AppBar(
         title: const Text('Showcase'),
       ),
