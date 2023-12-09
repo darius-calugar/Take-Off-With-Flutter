84702c5dfa047095a719d5749b2b65f7de3627cf
Darius Calugar <darius.calugar@rebeldot.com>
2023-12-09 17:35:43 +0200

    Made example 6 more interesting

--- take_off_with_flutter/lib/screens/counter_screen.dart
+++ take_off_with_flutter/lib/screens/counter_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:take_off_with_flutter/blocs/counter/counter_bloc.dart';
@@ -10,6 +12,7 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     final counterBloc = context.watch<CounterBloc>();
 
     return Scaffold(
@@ -23,51 +26,64 @@ class CounterScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           FloatingActionButton(
-            heroTag: 'minus_button',
-            onPressed: () => _onDecrementPressed(context),
-            child: const Text(
-              '-1',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            heroTag: 'minus_2',
+            onPressed: () => _onDecrementPressed(context, 2),
+            child: const Text('-2'),
           ),
           const SizedBox(
             width: 16,
           ),
           FloatingActionButton(
-            heroTag: 'add_button',
-            onPressed: () => _onIncrementPressed(context),
-            child: const Text(
-              '+2',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            heroTag: 'minus_1',
+            onPressed: () => _onDecrementPressed(context, 1),
+            child: const Text('-1'),
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          FloatingActionButton(
+            heroTag: 'add_1',
+            onPressed: () => _onIncrementPressed(context, 1),
+            child: const Text('+1'),
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          FloatingActionButton(
+            heroTag: 'add_2',
+            onPressed: () => _onIncrementPressed(context, 2),
+            child: const Text('+2'),
           ),
         ],
       ),
       body: Center(
-        child: Text(
-          '${counterBloc.state.counter}',
-          style: const TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+        child: SizedBox.square(
+          dimension: 50 + max(0, counterBloc.state.counter).toDouble(),
+          child: Material(
+            color: theme.colorScheme.primaryContainer,
+            shape: const CircleBorder(),
+            child: Center(
+              child: Text(
+                '${counterBloc.state.counter}',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
         ),
       ),
     );
   }
 
-  void _onDecrementPressed(BuildContext context) {
+  void _onDecrementPressed(BuildContext context, int value) {
     final counterBloc = context.read<CounterBloc>();
-    counterBloc.add(CounterDecrementedEvent(value: 1));
+    counterBloc.add(CounterDecrementedEvent(value: value));
   }
 
-  void _onIncrementPressed(BuildContext context) {
+  void _onIncrementPressed(BuildContext context, int value) {
     final counterBloc = context.read<CounterBloc>();
-    counterBloc.add(CounterIncrementedEvent(value: 2));
+    counterBloc.add(CounterIncrementedEvent(value: value));
   }
 }
